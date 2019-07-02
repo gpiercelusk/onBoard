35c96f20ed736972415a8b82bb454ce1ebefbb1a
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-07-02 19:44:11 -0400

    added validation  commented out unused models ATM

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-//@route   GET api/users
+//@route   post api/users
 //@desc    Test Route
 //@access  Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User route');
+  }
+);
 
 module.export = router;
