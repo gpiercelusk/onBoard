80f7fbb990100af81b0fd7dad56861a1142a302d
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-07-02 21:06:30 -0400

    Auth middleware added

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function(req, res, next) {
+  //get token from header
+  const token = req.header('x-auth-token');
+  //check if no token
+  if (!tokem) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+  //verify token
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
+const auth = require('../../middleware/auth');
 //@route   GET api/auth
 //@desc    Test Route
 //@access  Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await user.findById(req.user.id);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
