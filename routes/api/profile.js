73ae814817ab469609c79ff8fe072e07a8aa3da4
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-07-04 23:41:53 -0400

    added profile model working on profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,54 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
-//@route   GET api/profile
-//@desc    Test Route
-//@access  Public
-router.get('/', (req, res) => res.send('Profile route'));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
+//@route   GET api/profile/me
+//@desc    get current user profile
+//@access  Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+//@route   POST api/profile/
+//@desc    creat or update user profile
+//@access  Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status is required')
+        .not()
+        .isEmpty(),
+      check('games', 'Games are required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { location, status, games } = req.body;
+  }
+);
 module.exports = router;
