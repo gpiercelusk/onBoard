d96589ea489eeb936cc3b5622027b0488f828af0
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-07-06 13:52:29 -0400

    commiting to pull from master

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,33 @@
-import React from 'react';
-import './App.css';
-//import Link from "react-router-dom";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Landing from './pages/Landing';
-import SignUpPage from './pages/SignUpPage';
-import SignInPage from './pages/SignInPage';
-import Feed from './pages/Feed';
-import Profile from './pages/Profile';
+import React, { Fragment, useEffect } from 'react';
+//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+//import Routes from './components/routing/Routes';
 
-function App() {
+// Redux
+//import { Provider } from 'react-redux';
+//import store from './store';
+//import { loadUser } from './actions/auth';
+//import setAuthToken from './utils/setAuthToken';
+
+import './App.css';
+
+// if (localStorage.token) {
+//   setAuthToken(localStorage.token);
+// }
+
+const App = () => {
+  // useEffect(() => {
+  //   store.dispatch(loadUser());
+  // }, []);
+
   return (
-    <div className="App">
-      <div className="container">
-        <div className="wrapper">
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Landing} />
-              <Route exact path="/signuppage" component={SignUpPage} />
-              <Route exact path="/signinpage" component={SignInPage} />
-              <Route exact path="/feed" component={Feed} />
-              <Route exact path="/profile" component={Profile} />
-            </Switch>
-          </Router>
-        </div>
-      </div>
-    </div>
+    <Fragment>
+      <Navbar />
+      <Landing />
+    </Fragment>
   );
-}
+};
 
 export default App;
+
