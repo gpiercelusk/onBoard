0c4d8a9766dab553cf652f842a88f411eedf9cf4
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-07-07 16:02:44 -0400

    dashboard coming together
    added loading spinner

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -40,7 +40,6 @@ export default function (state = initialState, action) {
       return {
         ...state,
         profile: null,
-        repos: [],
         loading: false
       };
     default:
