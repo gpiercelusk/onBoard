2302d47491346ac421c62852a55405b72db665b4
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-07-06 20:21:48 -0400

    added an "alert" when passwords do not match

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert'
+import PropTypes from 'prop-types'
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -16,7 +19,7 @@ const Register = () => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      console.log('Passwords Do not match')
+      setAlert('Passwords do not match', 'danger')
     } else {
       console.log('Success')
     }
@@ -77,5 +80,7 @@ const Register = () => {
     </Fragment>
   );
 };
-
-export default Register
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+}
+export default connect(null, { setAlert })(Register)
