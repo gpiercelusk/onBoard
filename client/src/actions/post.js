c09bbd46207bc111ef25fb4272dff8aec8cf6c74
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-07-08 15:24:35 -0400

    added get post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -5,7 +5,8 @@ import {
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POST,
-  ADD_POST
+  ADD_POST,
+  GET_POST
 
 } from './types';
 
@@ -59,6 +60,7 @@ export const removeLike = id => async dispatch => {
     });
   }
 };
+
 // Delete post
 export const deletePost = id => async dispatch => {
   try {
@@ -95,6 +97,23 @@ export const addPost = formData => async dispatch => {
     });
 
     dispatch(setAlert('Post Created', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Get post
+export const getPost = id => async dispatch => {
+  try {
+    const res = await axios.get(`/api/posts/${id}`);
+
+    dispatch({
+      type: GET_POST,
+      payload: res.data
+    });
   } catch (err) {
     dispatch({
       type: POST_ERROR,
